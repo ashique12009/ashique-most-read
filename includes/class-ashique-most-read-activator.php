934d05b0c111237a4fb0ac37403125ee7b7e04d6
Ashique <ashique12009@gmail.com>
2022-10-06 16:13:51 +0600

    initial commit

---
+++ includes/class-ashique-most-read-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://ashique12009.blogspot.com/
+ * @since      1.0.0
+ *
+ * @package    Ashique_Most_Read
+ * @subpackage Ashique_Most_Read/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Ashique_Most_Read
+ * @subpackage Ashique_Most_Read/includes
+ * @author     khandoker Ashique Mahamud <ashique12009@gmail.com>
+ */
+class Ashique_Most_Read_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
