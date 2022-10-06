2b4f7018d621eefa9bbcc6add6e316ec35440d86
Ashique <ashique12009@gmail.com>
2022-10-06 23:17:59 +0600

    post number and days saved and affecting in frontend done

--- admin/partials/ashique-most-read-admin-settings-page.php
+++ admin/partials/ashique-most-read-admin-settings-page.php
@@ -9,8 +9,15 @@ if (!defined('ABSPATH')) {
         if (!wp_verify_nonce($_POST['most_read_post_number_settings_nonce_field'], 'most-read-post-number-settings-nonce' )) {
             die('Something went wrong!');
         }
+        elseif ($_POST['ashique_most_read_post_post_number'] < 1) {
+            die('Set at least 1 to post number');
+        }
+        elseif ($_POST['ashique_most_read_post_days_number'] < 1) {
+            die('Set at least 1 to days number');
+        }
         else {
             update_option( 'most_read_post_number', $_POST['ashique_most_read_post_post_number'] );
+            update_option( 'most_read_days_number', $_POST['ashique_most_read_post_days_number'] );
         }
     }
 
@@ -26,6 +33,14 @@ if (!defined('ABSPATH')) {
                     <th scope="row"><label for="ashique-most-read-post-post-number">How many posts you want to show: </label></th>
                     <td>
                         <input type="number" id="ashique-most-read-post-post-number" name="ashique_most_read_post_post_number" class="regular-text" value="<?php echo get_option( 'most_read_post_number', 4 );?>">
+                    </td>
+                </tr>
+            </tbody>
+            <tbody>
+                <tr>
+                    <th scope="row"><label for="ashique-most-read-post-days-number">How many days interval you want to see: </label></th>
+                    <td>
+                        <input type="number" id="ashique-most-read-post-days-number" name="ashique_most_read_post_days_number" class="regular-text" value="<?php echo get_option( 'most_read_days_number', 7 );?>">
                     </td>
                 </tr>
             </tbody>
--- public/class-ashique-most-read-public.php
+++ public/class-ashique-most-read-public.php
@@ -167,14 +167,18 @@ class Ashique_Most_Read_Public {
 
 		$table_name = $wpdb->prefix . 'ashique_most_read_posts';
 
+		// How many posts to show
+		$most_read_post_number = get_option( 'most_read_post_number', 4 );
+		$most_read_days_number = get_option( 'most_read_days_number', 7 );
+
 		$results = $wpdb->get_results("
 			SELECT post_id, SUM(read_counter) as read_counter  
 			FROM $table_name 
 			WHERE 
-			read_date >= DATE(NOW() - INTERVAL 7 DAY)  
+			read_date >= DATE(NOW() - INTERVAL $most_read_days_number DAY)  
 			GROUP BY post_id 
 			ORDER BY read_counter DESC 
-			LIMIT 4"
+			LIMIT $most_read_post_number"
 		);
 
 		$html = "<ul>";
@@ -196,14 +200,18 @@ class Ashique_Most_Read_Public {
 
 		$table_name = $wpdb->prefix . 'ashique_most_read_posts';
 
+		// How many posts to show
+		$most_read_post_number = get_option( 'most_read_post_number', 4 );
+		$most_read_days_number = get_option( 'most_read_days_number', 7 );
+
 		$results = $wpdb->get_results("
 			SELECT post_id, SUM(read_counter) as read_counter  
 			FROM $table_name 
 			WHERE 
-			read_date >= DATE(NOW() - INTERVAL 7 DAY)  
+			read_date >= DATE(NOW() - INTERVAL $most_read_days_number DAY)  
 			GROUP BY post_id 
 			ORDER BY read_counter DESC 
-			LIMIT 4"
+			LIMIT $most_read_post_number"
 		);
 
 		set_transient('most_read_posts', $results);
